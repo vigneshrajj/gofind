d83dd05d1dae9ddcce83bf649be1834e273e4829
Vignesh Raj <vigneshhrajj@gmail.com>
2024-11-02 20:35:40 +0530

    remove placeholder layout template

--- service/template_service.go
+++ service/template_service.go
@@ -8,25 +8,6 @@ import (
 	"github.com/vigneshrajj/gofind/models"
 )
 
-type MainLayoutData struct {
-	PageTitle string
-}
-
-func MainLayout(w http.ResponseWriter) {
-	data := MainLayoutData{
-		PageTitle: "Test title",
-	}
-	tmpl, err := template.ParseFiles("static/templates/layout.html")
-	if err != nil {
-		fmt.Fprint(w, "MainLayout Template not found.")
-		return
-	}
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		fmt.Fprint(w, "MainLayout Template couldn't be executed.")
-	}
-}
-
 type ListCommandsPageData struct {
 	Commands []models.Command
 }
