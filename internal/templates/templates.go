d258f4732e998433c3c146bc3657c3277d32dc05
Vignesh Raj <vigneshhrajj@gmail.com>
2024-11-05 15:17:41 +0530

    style: show individual arguments and hostname instead of raw query

--- internal/templates/templates.go
+++ internal/templates/templates.go
@@ -1,20 +1,99 @@
 package templates
 
 import (
-	"github.com/vigneshrajj/gofind/models"
 	"html/template"
 	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/vigneshrajj/gofind/models"
 )
 
-type ListCommandsPageData struct {
-	GroupedCommands map[models.CommandType][]models.Command
+type ArgType string
+
+const (
+	KeyVal ArgType = "keyval"
+	Num ArgType = "num"
+	Any ArgType = "any"
+)
+
+type CommandWithArgs struct {
+	models.Command
+
+	QueryHostname string
+	ArgType ArgType
+	ArgsKeyVal map[string]string
+	ArgsNum []int
 }
 
-func groupByType(commands []models.Command) map[models.CommandType][]models.Command {
-	groupedCommands := make(map[models.CommandType][]models.Command)
+type ListCommandsPageData struct {
+	GroupedCommands map[models.CommandType][]CommandWithArgs
+}
+
+func ExtractNumArgs(query string) []int {
+	re := regexp.MustCompile(`\{(\d+)\}`)
+	matches := re.FindAllStringSubmatch(query, -1)
+
+	var result []int
+
+	for _, match := range matches {
+		num, err := strconv.Atoi(match[1])
+		if err == nil {
+			result = append(result, num)
+		}
+	}
+
+	return result
+}
+
+func ExtractKeyValArgs(query string) map[string]string {
+	re := regexp.MustCompile(`\{([^\}]+)\}`)
+
+	matches := re.FindAllStringSubmatch(query, -1)
+
+	result := make(map[string]string)
+
+	for _, match := range matches {
+		keyValuePairs := match[1]
+		pairs := strings.Split(keyValuePairs, ",")
+		for _, pair := range pairs {
+			kv := strings.Split(pair, ":")
+			if len(kv) == 2 {
+				result[kv[0]] = kv[1]
+			}
+		}
+	}
+
+	return result
+}
+
+		
+
+func groupByType(commands []models.Command) map[models.CommandType][]CommandWithArgs {
+	groupedCommands := make(map[models.CommandType][]CommandWithArgs)
 
 	for _, command := range commands {
-		groupedCommands[command.Type] = append(groupedCommands[command.Type], command)
+		uri, err := url.Parse(command.Query)
+		if err != nil {
+			return groupedCommands
+		}
+		commandWithArgs := CommandWithArgs{
+			Command: command,
+			ArgsNum: ExtractNumArgs(command.Query),
+			ArgsKeyVal: ExtractKeyValArgs(command.Query),
+		}
+		if len(commandWithArgs.ArgsKeyVal) > 0 {
+			commandWithArgs.ArgType = KeyVal
+		} else if (len(commandWithArgs.ArgsNum) > 0) {
+			commandWithArgs.ArgType = Num
+		} else {
+			commandWithArgs.ArgType = Any
+		}
+
+		commandWithArgs.QueryHostname = uri.Hostname()
+		groupedCommands[command.Type] = append(groupedCommands[command.Type], commandWithArgs)
 	}
 
 	return groupedCommands
